ca136abee0b5c671889c36f413dbdbcb2148984d
EmmaFC <emmafecor@hotmail.com>
2022-02-02 00:45:36 +0100

    kata 04 changes

--- JS-Transpose/kata_04.js
+++ JS-Transpose/kata_04.js
@@ -1,46 +1,46 @@
 export const transpose = (arr) => {
     let new_matrix = [];
-    console.log(arr)
- 
+
     if(arr.length = 1){
+
     arr.forEach((item)=>{
-        let newArray = Array.from(item)
-        for(let i=0; i<newArray.length; i++){
-            let character = newArray[i]
-            new_matrix.push(character) 
-       }
-     })
+      new_matrix = Array.from(item)
+      return new_matrix
+    }) 
+   }
+
+
+if(arr.length > 1){
+
+   arr.forEach((item)=>{
+
+   let newArray = Array.from(item)
+
+ 
+      
+   for(let i=0; i<newArray.length; i++){
+
+       let character = newArray [i]
+      
+
+       if( new_matrix[i] != undefined){
+       new_matrix[i] = new_matrix[i] + character
+       return new_matrix
+
+        }else{
+            new_matrix[i] = character
+            return new_matrix
+
+        }
     }
 
-    if(arr.length > 1){
-
-    arr.forEach((item)=>{
-
-       let newArray = Array.from(item)
-
-
-       for(let i=0; i<newArray.length; i++){
-
-           let character = newArray[i]
-           console.log(character)
-
-           if( new_matrix[i] != undefined){
-           new_matrix[i] = new_matrix[i] + character
-           return new_matrix
-
-            }else{
-                new_matrix[i] = character
-                return new_matrix
-
-            }
-        }
-        
-        
-          
-    })
+     })
 }
-    console.log(new_matrix)
-    return new_matrix
+      
+
+
+
+return new_matrix
     
 };
   
